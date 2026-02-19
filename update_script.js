285a698c08908019ada48c0d13757cc453c93b6f
tsejoven <tsejoven@gmail.com>
2026-02-19 15:45:06 +0800

    Update update_script.js

--- update_script.js
+++ update_script.js
@@ -38,6 +38,10 @@ const SOURCE_URLS = [
   "https://raw.githubusercontent.com/YueChan/Live/main/IPTV.m3u",
   "https://raw.githubusercontent.com/Guovern/iptv/master/docs/iptv.m3u",
   "https://raw.githubusercontent.com/suxuang/myIPTV/main/ipv4.m3u",
+  "https://php.946985.filegear-sg.me/jackTV.m3u"
+  "http://rihou.cc:555/gggg.nzk"
+  "https://ds65.tv1288.xyz"
+  "https://gh-proxy.org/https://raw.githubusercontent.com/fafa002/yf2025/refs/heads/main/yiyifafa.txt"
   "https://raw.githubusercontent.com/Jsnzkpg/Jsnzkpg/refs/heads/Jsnzkpg/Jsnzkpg1.m3u",
   // 增加針對港澳台的特定源
   "https://raw.githubusercontent.com/Moexin/IPTV/main/TV.m3u",
